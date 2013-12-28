cbd99d5e2a5b6b002ac8dd40cd22216a3e551939
cryptoshekel <cryptoshekel@hashmail.com>
2013-12-28 13:02:01 -0500

    branding intensifies

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2013-12-28 01:40:19 -0500"
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("SHKL");
+    case mBTC: return QString("mSHKL");
+    case uBTC: return QString::fromUtf8("μSHKL");
     default: return QString("???");
     }
 }
